06eb87ad08d64638a04e7ebc1ff63ab759175779
RolandCzinege <roland.czinege@sundroid.hu>
2023-04-01 22:29:13 +0200

    mvc client added

--- MLModel1_WebApi1/Program.cs
+++ MLModel1_WebApi1/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.ML;
-using Microsoft.OpenApi.Models;
 using MLModel1_WebApi1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPredictionEnginePool<MLModel1.ModelInput, MLModel1.ModelOutput>()
     .FromFile("MLModel1.mlnet");
 
+builder.Services.AddMvc();
 builder.Services.AddControllersWithViews();
 builder.Services.AddEndpointsApiExplorer();
 
@@ -14,24 +14,43 @@ builder.Services.AddTransient<IPredicationService, PredicationService>();
 builder.Services.AddTransient<IDrawingService, DrawingService>();
 builder.Services.AddTransient<IDrawingFancyService, DrawingFancyService>();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Description = "Docs for my API", Version = "v1" });
-});
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Description = "Docs for my API", Version = "v1" });
+//});
 var app = builder.Build();
 
 app.UseRouting();
-app.UseSwagger();
+//app.UseSwagger();
 
-app.UseSwaggerUI(c =>
-{
-    c.RoutePrefix = "";
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-});
+//app.UseSwaggerUI(c =>
+//{
+//    c.RoutePrefix = "";
+//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+//});
+
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapDefaultControllerRoute();
+//});
+
+//app.Use(async (context, next) =>
+//{
+//    if (context.Request.Path == "/" || context.Request.Path == "/index.html")
+//    {
+//        context.Request.Path = "/ObjectDetection";
+//    }
+//    await next.Invoke();
+//});
 
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapDefaultControllerRoute();
+    endpoints.MapControllerRoute(
+           name: "default",
+           pattern: "{controller=ObjectDetection}/{action=Index}/{id?}");
+
+    endpoints.MapControllers();
+
 });
 
 app.Run();
