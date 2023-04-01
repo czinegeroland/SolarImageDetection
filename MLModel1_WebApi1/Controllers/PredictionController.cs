2896a73ca409878c7a79c76215d259cceb4cffb0
RolandCzinege <roland.czinege@sundroid.hu>
2023-04-01 18:35:02 +0200

    working drawing

--- MLModel1_WebApi1/Controllers/PredictionController.cs
+++ MLModel1_WebApi1/Controllers/PredictionController.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.ML;
 using Microsoft.ML.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Drawing.Printing;
 using static MLModel1.ModelOutput;
 using Color = System.Drawing.Color;
 
@@ -59,10 +60,39 @@ namespace MLModel1_WebApi1.Controllers
             return File(imageBytes, "image/jpeg");
         }
 
+        //public static byte[] DrawRectangles(MemoryStream inputImageStream, List<BoundingBox> boundingBoxes)
+        //{
+        //    Image inputImage = Image.FromStream(inputImageStream);
+        //    Bitmap outputImage = new Bitmap(inputImage.Width, inputImage.Height, PixelFormat.Format32bppArgb);
+
+        //    var rectangles = new List<RectangleF>();
+
+        //    using (Graphics graphics = Graphics.FromImage(outputImage))
+        //    {
+        //        graphics.DrawImage(inputImage, 0, 0, inputImage.Width, inputImage.Height);
+
+        //        Pen pen = new Pen(Color.Red, 2);
+
+        //        foreach (var box in boundingBoxes)
+        //        {
+        //            RectangleF rect = BoundingBoxToRectangle(box);
+        //            graphics.DrawRectangle(pen, rect);
+        //        }
+        //    }
+
+        //    using (MemoryStream outputStream = new MemoryStream())
+        //    {
+        //        outputImage.Save(outputStream, ImageFormat.Jpeg);
+        //        return outputStream.ToArray();
+        //    }
+        //}
+
         public static byte[] DrawRectangles(MemoryStream inputImageStream, List<BoundingBox> boundingBoxes)
         {
             Image inputImage = Image.FromStream(inputImageStream);
             Bitmap outputImage = new Bitmap(inputImage.Width, inputImage.Height, PixelFormat.Format32bppArgb);
+
+            var rectangles = new List<RectangleF>();
 
             using (Graphics graphics = Graphics.FromImage(outputImage))
             {
@@ -72,8 +102,13 @@ namespace MLModel1_WebApi1.Controllers
 
                 foreach (var box in boundingBoxes)
                 {
-                    Rectangle rect = BoundingBoxToRectangle(box);
-                    graphics.DrawRectangle(pen, rect);
+                    RectangleF rect = BoundingBoxToRectangle(box);
+                    rectangles.Add(rect);
+                }
+
+                foreach (var item in NonMaximumSuppression(rectangles, 0.1f))
+                {
+                    graphics.DrawRectangle(pen, item);
                 }
             }
 
@@ -84,145 +119,96 @@ namespace MLModel1_WebApi1.Controllers
             }
         }
 
-            //public static byte[] DrawRectangles(MemoryStream inputImageStream, List<MLModel1.ModelOutput.BoundingBox> boundingBoxes)
-            //{
-            //    System.Drawing.Image image = System.Drawing.Image.FromStream(inputImageStream);
-
-            //    using (Graphics graphics = Graphics.FromImage(image))
-            //    {
-            //        Pen pen = new Pen(Color.Red, 2);
-
-            //        foreach (var box in boundingBoxes)
-            //        {
-            //            Rectangle rect = BoundingBoxToRectangle(box);
-
-            //            graphics.DrawRectangle(pen, rect);
-            //        }
-            //    }
-
-            //    using (MemoryStream outputStream = new MemoryStream())
-            //    {
-            //        image.Save(outputStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-            //        return outputStream.ToArray();
-            //    }
-            //}
-
-        public static Rectangle BoundingBoxToRectangle(BoundingBox boundingBox)
+        public static RectangleF BoundingBoxToRectangle(BoundingBox boundingBox)
         {
-            int x = (int)boundingBox.Left;
-            int y = (int)boundingBox.Top;
-            int width = (int)(boundingBox.Right - boundingBox.Left);
-            int height = (int)(boundingBox.Bottom - boundingBox.Top);
+            var left = (boundingBox.Left / 800) * 500;
+            var top = (boundingBox.Top / 600) * 500;
+            var right = (boundingBox.Right / 800) * 500;
+            var bottom = (boundingBox.Bottom / 600) * 500;
 
-            var x1 = (int)boundingBox.Top;
-            var y1 = (int)boundingBox.Left;
-            var x2 = (int)boundingBox.Right;
-            var y2 = (int)boundingBox.Bottom;
-
-            return new Rectangle(x1, y1, x2 - x1, y2 - y1);
-
-
+            return RectangleF.FromLTRB(left, top, right, bottom);
         }
 
-        //public System.Drawing.Image DrawBoundingBox(MemoryStream ms, List<MLModel1.ModelOutput.BoundingBox> filteredBoxes)
+        public static List<RectangleF> NonMaximumSuppression(List<RectangleF> rectangles, float iouThreshold)
+        {
+            List<RectangleF> outputRectangles = new List<RectangleF>();
+
+            // Sort rectangles by area in descending order
+            var sortedRectangles = rectangles.OrderByDescending(r => r.Width * r.Height).ToList();
+
+            while (sortedRectangles.Count > 0)
+            {
+                RectangleF currentRect = sortedRectangles[0];
+                outputRectangles.Add(currentRect);
+                sortedRectangles.RemoveAt(0);
+
+                for (int i = sortedRectangles.Count - 1; i >= 0; i--)
+                {
+                    if (ComputeIoU(currentRect, sortedRectangles[i]) > iouThreshold)
+                    {
+                        sortedRectangles.RemoveAt(i);
+                    }
+                }
+            }
+
+            return outputRectangles;
+        }
+
+        public static float ComputeIoU(RectangleF rect1, RectangleF rect2)
+        {
+            float xA = Math.Max(rect1.Left, rect2.Left);
+            float yA = Math.Max(rect1.Top, rect2.Top);
+            float xB = Math.Min(rect1.Right, rect2.Right);
+            float yB = Math.Min(rect1.Bottom, rect2.Bottom);
+
+            float intersectionArea = Math.Max(0, xB - xA) * Math.Max(0, yB - yA);
+            float rect1Area = rect1.Width * rect1.Height;
+            float rect2Area = rect2.Width * rect2.Height;
+
+            float unionArea = rect1Area + rect2Area - intersectionArea;
+
+            return intersectionArea / unionArea;
+        }
+
+        //public static List<BoundingBox> NonMaximumSuppression(List<BoundingBox> boundingBoxes, float iouThreshold)
         //{
-        //    System.Drawing.Image image = System.Drawing.Image.FromStream(ms);
-        //    var originalHeight = image.Height;
-        //    var originalWidth = image.Width;
-        //    foreach (var box in filteredBoxes)
+        //    List<BoundingBox> outputBoxes = new List<BoundingBox>();
+
+        //    // Sort bounding boxes by score in descending order
+        //    var sortedBoxes = boundingBoxes.OrderByDescending(b => b.Score).ToList();
+
+        //    while (sortedBoxes.Count > 0)
         //    {
-        //        //// process output boxes
-        //        var x = (uint)Math.Max(box.Dimensions.X, 0);
-        //        var y = (uint)Math.Max(box.Dimensions.Y, 0);
-        //        var width = (uint)Math.Min(originalWidth - x, box.Dimensions.Width);
-        //        var height = (uint)Math.Min(originalHeight - y, box.Dimensions.Height);
+        //        BoundingBox currentBox = sortedBoxes[0];
+        //        outputBoxes.Add(currentBox);
+        //        sortedBoxes.RemoveAt(0);
 
-        //        // fit to current image size
-        //        x = (uint)originalWidth * x / ImageSettings.imageWidth;
-        //        y = (uint)originalHeight * y / ImageSettings.imageHeight;
-        //        width = (uint)originalWidth * width / ImageSettings.imageWidth;
-        //        height = (uint)originalHeight * height / ImageSettings.imageHeight;
-
-        //        using (Graphics thumbnailGraphic = Graphics.FromImage(image))
+        //        for (int i = sortedBoxes.Count - 1; i >= 0; i--)
         //        {
-        //            thumbnailGraphic.CompositingQuality = CompositingQuality.HighQuality;
-        //            thumbnailGraphic.SmoothingMode = SmoothingMode.HighQuality;
-        //            thumbnailGraphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
-
-        //            // Define Text Options
-        //            Font drawFont = new Font("Arial", 12, FontStyle.Bold);
-        //            SizeF size = thumbnailGraphic.MeasureString(box.Description, drawFont);
-        //            SolidBrush fontBrush = new SolidBrush(Color.Black);
-        //            Point atPoint = new Point((int)x, (int)y - (int)size.Height - 1);
-
-        //            // Define BoundingBox options
-        //            Pen pen = new Pen(box.BoxColor, 3.2f);
-        //            SolidBrush colorBrush = new SolidBrush(box.BoxColor);
-
-        //            // Draw text on image 
-        //            thumbnailGraphic.FillRectangle(colorBrush, (int)x, (int)(y - size.Height - 1), (int)size.Width, (int)size.Height);
-        //            thumbnailGraphic.DrawString(box.Description, drawFont, fontBrush, atPoint);
-
-        //            // Draw bounding box on image
-        //            thumbnailGraphic.DrawRectangle(pen, x, y, width, height);
+        //            if (ComputeIoU(currentBox, sortedBoxes[i]) > iouThreshold)
+        //            {
+        //                sortedBoxes.RemoveAt(i);
+        //            }
         //        }
         //    }
-        //    return image;
+
+        //    return outputBoxes;
         //}
 
-        //private void DrawLabel(string label, Image<Rgba32> img, float x1, float y1)
+        //public static float ComputeIoU(BoundingBox box1, BoundingBox box2)
         //{
-        //    float fontSize = 12; // Adjust the font size if needed
-        //    var font = SixLabors.Fonts.SystemFonts.CreateFont("Arial", fontSize);
+        //    float xA = Math.Max(box1.Left, box2.Left);
+        //    float yA = Math.Max(box1.Top, box2.Top);
+        //    float xB = Math.Min(box1.Right, box2.Right);
+        //    float yB = Math.Min(box1.Bottom, box2.Bottom);
 
-        //    var rendererOptions = new RendererOptions(font)
-        //    {
-        //        HorizontalAlignment= HorizontalAlignment.Left,
-        //        VerticalAlignment = VerticalAlignment.Top,
-        //    };
-        //    var labelColor = SixLabors.ImageSharp.Color.White;
-        //    img.Mutate(ctx => ctx.DrawText(label, font, labelColor, new SixLabors.ImageSharp.PointF(x1, y1 - fontSize)));
-        //}
+        //    float intersectionArea = Math.Max(0, xB - xA) * Math.Max(0, yB - yA);
+        //    float box1Area = (box1.Right - box1.Left) * (box1.Bottom - box1.Top);
+        //    float box2Area = (box2.Right - box2.Left) * (box2.Bottom - box2.Top);
 
-        //private List<int> NonMaximumSuppression(List<SixLabors.ImageSharp.RectangleF> boxes, float[] scores, float threshold)
-        //{
-        //    List<int> indices = Enumerable.Range(0, boxes.Count).ToList();
-        //    indices.Sort((i, j) => scores[j].CompareTo(scores[i]));
+        //    float unionArea = box1Area + box2Area - intersectionArea;
 
-        //    List<int> keep = new List<int>();
-
-        //    while (indices.Count > 0)
-        //    {
-        //        int current = indices[0];
-        //        keep.Add(current);
-        //        indices.RemoveAt(0);
-
-        //        List<int> removeIndices = new List<int>();
-        //        for (int i = 0; i < indices.Count; i++)
-        //        {
-        //            int index = indices[i];
-        //            float iou = ComputeIoU(boxes[current], boxes[index]);
-        //            if (iou >= threshold)
-        //                removeIndices.Add(i);
-        //        }
-
-        //        indices = indices.Where((_, i) => !removeIndices.Contains(i)).ToList();
-        //    }
-
-        //    return keep;
-        //}
-
-        //private float ComputeIoU(SixLabors.ImageSharp.RectangleF rect1, SixLabors.ImageSharp.RectangleF rect2)
-        //{
-        //    float xA = Math.Max(rect1.Left, rect2.Left);
-        //    float yA = Math.Max(rect1.Top, rect2.Top);
-        //    float xB = Math.Min(rect1.Right, rect2.Right);
-        //    float yB = Math.Min(rect1.Bottom, rect2.Bottom);
-
-        //    float interArea = Math.Max(0, xB - xA) * Math.Max(0, yB - yA);
-        //    float unionArea = rect1.Width * rect1.Height + rect2.Width * rect2.Height - interArea;
-
-        //    return interArea / unionArea;
+        //    return intersectionArea / unionArea;
         //}
     }
 }
