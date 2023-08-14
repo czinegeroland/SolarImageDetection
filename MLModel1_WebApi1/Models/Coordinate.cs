f34fa9249bff1b3c889834be21836ac9f61b7158
RolandCzinege <roland.czinege@sundroid.hu>
2023-08-14 21:22:25 +0200

    Solar image download feature

---
+++ MLModel1_WebApi1/Models/Coordinate.cs
@@ -0,0 +1,13 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace MLModel1_WebApi1.Models
+{
+    public class Coordinate
+    {
+        [Name("latitude")]
+        public double Latitude { get; set; }
+
+        [Name("longitude")]
+        public double Longitude { get; set; }
+    }
+}
